d0c1193e7d5bec7130ee7350c774f8f3c0c76c4c
hugo <hugordura@hotmail.com>
2023-02-12 18:03:20 -0400

    Polimorfismos,abstração e criacao de contas

---
+++ src/main/kotlin/Analista.kt
@@ -0,0 +1,25 @@
+class Gerente(
+    nome:String,
+    cpf:String,
+    salario:Double,
+    val senha:Int
+) :Funcionario(
+    nome = nome,
+    cpf = cpf,
+    salario = salario
+){
+    //comando override permite editar um metodo qu veio da classe mae
+    override fun bonificacao(): Double {
+        //comando super faz a chamada da funcao na classe pai, conceito de heranca
+        return super.bonificacao() + salario
+    }
+    fun autentica(senha:Int): Boolean {
+        if(this.senha==senha){
+            return true
+        }
+        return false
+//        if(tipo ==1){
+//            //pode autenticar
+//        }
+    }
+}
