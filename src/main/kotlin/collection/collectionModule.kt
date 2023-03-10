c9070adbf2b70a8ae7d66df383daa556d5860cd8
hugo <hugordura@hotmail.com>
2023-03-09 21:46:05 -0400

    olar

--- src/main/kotlin/collection/collectionModule.kt
+++ src/main/kotlin/collection/collectionModule.kt
@@ -4,6 +4,23 @@ fun collectionModule(){
     //testaCollection()
     //testaSetAndCollection()
     //testaMap2()
+
+    //associate
+  val pedidos=  listOf(
+        Pedido(1,20.00),
+        Pedido(2,30.00),
+        Pedido(3,30.00)
+    )
+
+    println(pedidos)
+    val pedidosMapeados:Map<Int,Pedido> = pedidos.associate { pedido:Pedido->
+        Pair(pedido.numero,pedido)
+    }
+    val pedidosMapeados2:Map<Int,Pedido> = pedidos.associateBy { pedido:Pedido->
+        pedido.numero //so precisa informar a chave que j[a vai fazer o pair
+    }
+    println(pedidosMapeados)
 }
 
 
+data class Pedido(val numero:Int, val valor:Double)
